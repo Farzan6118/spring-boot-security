d92e4793cd8d90b8ee3cc00b863771a02ee8dc7f
farzan <farzan.6118@gmail.com>
2025-12-07 13:53:34 +0330

    final changes

---
+++ src/main/java/com/example/security/config/OpenAPIConfig.java
@@ -0,0 +1,30 @@
+package com.example.security.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Security Boot Sample")
+                        .version("1.0")
+                        .description("Spring Security Boot Sample with Basic Auth")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("basicAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("basic")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"));
+    }
+}
